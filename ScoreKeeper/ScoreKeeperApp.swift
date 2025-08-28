5bec1617f2fb2cab7a379a70c3967e39e8bfe69e
Sean Joyce <seanthomasjoyce@icloud.com>
2025-08-28 18:47:19 +0100

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Sean Joyce on 28/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
